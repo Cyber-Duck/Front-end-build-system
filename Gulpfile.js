8bbe7fe1f573793ddfb7204c2f2c83dddd5e5360
Ramon Lapenta <ramono@gmail.com>
2019-04-19 14:02:08 -0600

    Upgraded build system for Gulp4

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,25 +3,20 @@
  * Cyber-Duck Ltd - www.cyber-duck.co.uk
  */
 
-const gulp = require('gulp'),
-    del = require('del'),
-    autoprefixer = require('gulp-autoprefixer'),
-    concat = require('gulp-concat'),
-    minify = require('gulp-cssnano'),
-    tinypng = require('gulp-tinypng'),
-    rename = require("gulp-rename"),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    watch = require('gulp-watch'),
-    eslint = require('gulp-eslint'),
-    csslint = require('gulp-stylelint'),
-    header = require('gulp-header'),
-    footer = require('gulp-footer'),
-    babel = require("gulp-babel"),
-    pkg = require('./package.json');
-    // sync = require('browser-sync').create()
-
+const gulp = require('gulp');
+const del = require('del');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const minify = require('gulp-cssnano');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const inject = require('gulp-inject-string');
+const header = require('gulp-header');
+const footer = require('gulp-footer');
+const babel = require('gulp-babel');
+const pkg = require('./package.json');
 
 /*
  * Main configuration object
@@ -31,14 +26,21 @@ const config = {
     jsSrc: './public/js/src',
     jsDest: './public/js/min',
     cssDir: './public/css',
-    tplDir: '',
-    imgSrc: './public/img'
+    tplDir: './public/',
+    tplName: 'index.html',
+    imgSrc: './public/img',
+    tinyPngApiKey: '',
+    injectAssets: true
 };
 
-
+const jsFiles = [
+    config.jsSrc + '/main.js', 
+    config.jsSrc + '/another.js', 
+    config.jsSrc + '/test.js'
+];
 
 /*
- * Current date 
+ * Current date
  */
 let date = new Date();
 let day = String('00' + date.getDate()).slice(-2);
@@ -51,68 +53,56 @@ let second = String('00' + date.getSeconds()).slice(-2);
 let now = `${day}/${month}/${year} @ ${hour}:${minute}`;
 let nowHash = `${year}${month}${day}${hour}${minute}${second}`;
 
-
-
 /*
  * Compile Sass for development
  * with sourcemaps and not minified
  */
-gulp.task('style-dev', () => {
+function cssDev () {
     'use strict';
-    return gulp.src(config.scssDir + '/*.scss')
+    return gulp
+        .src(config.scssDir + '/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .on('error', sass.logError)
-        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(autoprefixer({cascade: false}))
         .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest(config.cssDir));
-        // .pipe(sync.stream());
-});
-
-
+};
 
 /*
  * Compile Sass for production
  * with no sourcemaps and minified
  */
-gulp.task('style', () => {
+function css () {
     'use strict';
     return gulp.src(config.scssDir + '/*.scss')
         .pipe(sass())
         .on('error', sass.logError)
-        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(minify({
             zindex: false, 
             discardComments: {
                 removeAll: true
             }
         }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(gulp.dest(config.cssDir));
-});
-
-
+};
 
 /*
- * Inject stylesheet and scripts to file
+ * Inject stylesheet to file
  */
-gulp.task('injectCss', () => {
+function injectCss () {
     'use strict';
     let stylesheet = RegExp('style.([0-9]+).css');
-    return gulp.src(config.tplDir + 'index.php')
+    return gulp.src(config.tplDir + config.tplName)
         .pipe(inject.replace(stylesheet, 'style.' + nowHash + '.css'))
         .pipe(gulp.dest(config.tplDir));
-});
-gulp.task('injectJs', () => {
-    'use strict';
-    let scriptMin = RegExp('scripts.([0-9]+).min.js');
-    let script = RegExp('scripts.([0-9]+).js');
-    return gulp.src(config.tplDir + 'index.php')
-        .pipe(inject.replace(scriptMin, 'scripts.' + nowHash + '.min.js'))
-        .pipe(inject.replace(script, 'scripts.' + nowHash + '.js'))
-        .pipe(gulp.dest(config.tplDir));
-});
-
-
+};
 
 /*
  * Banner for JS file
@@ -126,8 +116,6 @@ let topBanner = `/*
 $(document).ready(function () {
 `;
 
-
-
 /*
  * End of JS file
  */
@@ -137,19 +125,12 @@ let bottomBanner = `
  */
 });`;
 
-
-
-
 /*
  * Concatenate and transpile JS files
  */
-gulp.task('js', () => {
+function js () {
     'use strict';
-    return gulp.src([
-            config.jsSrc + '/main.js', 
-            config.jsSrc + '/another.js', 
-            config.jsSrc + '/test.js'
-        ])
+    return gulp.src(jsFiles)
         .pipe(sourcemaps.init())
         .pipe(concat('scripts.js'))
         .pipe(babel())
@@ -161,144 +142,115 @@ gulp.task('js', () => {
         .pipe(footer(bottomBanner))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.jsDest));
-});
-
-
+};
 
 /*
  * Compress and rename JS files
+ * @todo: add requirements (js)
  */
-gulp.task('compress', ['js'], () => {
+function compress () {
     'use strict';
     return gulp.src(config.jsDest + '/scripts.js')
         .pipe(rename('scripts.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(config.jsDest));
-});
-
-
-gulp.task('lint', () => {
-    return gulp.src(config.jsSrc + '/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failOnError());
-});
-
-
+};
 
 /*
- * Lint CSS/Scss using Style Lint
+ * Inject scripts to file
  */
-gulp.task('lint-css', function lintCssTask() {
-    return gulp.src(config.scssDir + '/**/*.scss')
-        .pipe(csslint({
-            failAfterError: true,
-            reportOutputDir: 'reports/lint',
-            reporters: [
-                {formatter: 'verbose', console: true},
-                {formatter: 'json', save: 'report.json'},
-            ],
-        debug: true
-    }));
-});
-
+function injectJs () {
+    'use strict';
+    let scriptMin = RegExp('scripts.([0-9]+).min.js');
+    let script = RegExp('scripts.([0-9]+).js');
+    return gulp.src(config.tplDir + config.tplName)
+        .pipe(inject.replace(scriptMin, 'scripts.' + nowHash + '.min.js'))
+        .pipe(inject.replace(script, 'scripts.' + nowHash + '.js'))
+        .pipe(gulp.dest(config.tplDir));
+};
 
 /*
  * Clean non used JS files, and sourcemaps for production
+ * @todo: needs to have the required tasks (js, compress)
  */
-gulp.task('clean', ['style', 'compress'], () => {
+async function cleanCss () {
     'use strict';
-    del(config.cssDir + '/maps/*');
-    del(config.cssDir + '/maps/');
-    del(config.jsDest + '/scripts.js');
-    del(config.jsDest + '/scripts.js.map');
-});
+    await del([
+        config.cssDir + '/maps/*',
+        config.cssDir + '/maps/'
+    ]);
+};
 
-
+async function cleanJs () {
+    'use strict';
+    await del([
+        // config.jsDest + '/scripts.js', // File might be needed
+        config.jsDest + '/scripts.js.map'
+    ]);
+};
 
 /*
  * Optimise images using TinyPNG API
  */
-gulp.task('tinypng', function () {
+function tinypng () {
     'use strict';
     return gulp.src([
         config.imgSrc + '/**/*.png',
         config.imgSrc + '/**/*.jpg',
         config.imgSrc + '/**/*.jpeg'
     ])
-        .pipe(tinypng('API_KEY'))
+        .pipe(tinypng(config.tinyPngApiKey))
         .pipe(gulp.dest(config.imgSrc));
-});
-
-
-
-/*
- * Production task
- * Compiles Sass with no sourcemaps, minifies CSS
- * Compress JS
- * Clean sourcemaps and uncompressed JS
-*/
-gulp.task('build', () => {
-    'use strict';
-    gulp.start('style');
-    gulp.start('compress');
-    // gulp.start('injectCss');
-    // gulp.start('injectJs');
-    gulp.start('clean');
-});
-
-
-
-/*
- * Reload task for JS files used by BrowserSync
- */
-// gulp.task('js-sync', ['js'], () => {
-//     'use strict';
-//     sync.reload();
-// });
-
-
-
-/*
- * BrowserSync task, contains watchers for Sass, JS and Templates
- */
-// gulp.task('browsersync', ['style-dev', 'js'], () => {
-//     'use strict';
-//     sync.init({
-//         proxy: "domain.dev",
-//         browser: "google chrome"
-//     });
-//     gulp.watch(config.tplDir + '/**/*.ss').on('change', sync.reload);
-//     gulp.watch(config.scssDir + '/**/*.scss', ['style-dev']);
-//     gulp.watch(config.jsDir + '/src/*.js', ['js-sync']);
-// });
-
-
+};
 
 /*
  * Development mode, watching for changes
  * This mode creates non compressed CSS, non compressed JS
  * and creates sourcemaps.
 */
-gulp.task('watch', () => {
+function devWatch () {
     'use strict';
-    // Development task for compiling Sass
-    watch(config.scssDir + '/**/*.scss', () => {
-        gulp.start('style-dev');
-        // gulp.start('injectCss');
-    });
-
-    // Concatenating JS files, but not compressing
-    watch(config.jsSrc + '/*.js', () => {
-        gulp.start('js');
-        // gulp.start('injectJs');
-    });
-});
-
-
-
+    if (config.injectAssets) {
+        gulp.watch(config.scssDir + '/**/*.scss', gulp.series(cssDev, injectCss));
+        gulp.watch(config.jsSrc + '/*.js', gulp.series(js, injectJs));
+    } else {
+        gulp.watch(config.scssDir + '/**/*.scss', cssDev);
+        gulp.watch(config.jsSrc + '/*.js', js);
+    }
+};
 
 /*
- * Default task, runs the build task
+ * Compiles Sass with no sourcemaps, minifies CSS
+ * Compress JS
+*/
+function build (done) {
+    'use strict';
+    if (config.injectAssets) {
+        return gulp.series(
+            gulp.parallel(gulp.series(css, injectCss, cleanCss)),
+            gulp.parallel(gulp.series(js, compress, injectJs, cleanJs))
+        )(done);
+    } else {
+        return gulp.series(
+            gulp.parallel(gulp.series(css, cleanCss)),
+            gulp.parallel(gulp.series(js, compress, cleanJs))
+        )(done);
+    }
+};
+
+exports.cssDev = cssDev;
+exports.css = css;
+exports.injectCss = injectCss;
+exports.js = js;
+exports.compress = compress;
+exports.injectJs = injectJs;
+exports.cleanCss = cleanCss;
+exports.cleanJs = cleanJs;
+exports.tinypng = tinypng;
+exports.devWatch = devWatch;
+exports.build = build;
+
+/*
+ * Default task, runs the watch task
  */
-gulp.task('default', ['build']);
+gulp.task('default', gulp.parallel(devWatch));
